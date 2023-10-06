36813bb6f1c0d710ab4583533a8bf45f9eda8309
hilarious3 <shahzad20022002@gmail.com>
2023-10-06 09:44:52 +0530

    implementing login tocontact page

--- src/form.js
+++ src/form.js
@@ -1,46 +1,68 @@
-import React from 'react';
-import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+import React, { useRef } from "react";
+import emailjs from "@emailjs/browser";
+import { StyleSheet, Text, View, TextInput, Button } from "react-native";
 
 export default function Form() {
+  const [user_name, setName] = React.useState("");
+  const [user_email, setEmail] = React.useState("");
+  const [user_phone, setPhone] = React.useState("");
+  const [message, setMessage] = React.useState("");
 
-  const [name, setName] = React.useState('');
-  const [email, setEmail] = React.useState('');
-  const [phone, setPhone] = React.useState('');  
-  const [message, setMessage] = React.useState('');
+  const form = useRef();
 
-  const handleSubmit = () => {
-    // submit form data
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    emailjs
+      .sendForm(
+        "service_csy00tb",
+        "template_cmolh24",
+        form.current,
+        "NQF0bljVzFj-2Zzqv"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+          console.log("message sent");
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
   };
 
   return (
-    <View style={styles.container}>
+    <View style={styles.container} ref={form}>
       <Text style={styles.title}>Contact Us</Text>
-      <TextInput 
+      <TextInput
         style={styles.input}
         placeholder="Name"
-        value={name}
-        onChangeText={text => setName(text)}
-      />  
+        value={user_name}
+        onChangeText={(text) => setName(text)}
+        
+      />
       <TextInput
         style={styles.input}
         placeholder="Email"
-        value={email}
-        onChangeText={text => setEmail(text)}  
+        value={user_email}
+        onChangeText={(text) => setEmail(text)}
+        keyboardType="email-address"
       />
       <TextInput
         style={styles.input}
         placeholder="Phone"
-        value={phone}
-        onChangeText={text => setPhone(text)}
-        keyboardType="phone-pad" 
+        value={user_phone}
+        onChangeText={(text) => setPhone(text)}
+        keyboardType="phone-pad"
+        
       />
       <TextInput
         style={styles.input}
         placeholder="Message"
         value={message}
-        onChangeText={text => setMessage(text)}
+        onChangeText={(text) => setMessage(text)}
         multiline
         numberOfLines={4}
+        
       />
       <Button title="Submit" onPress={handleSubmit} />
     </View>
@@ -50,19 +72,19 @@ export default function Form() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',    
-    padding: 20
+    backgroundColor: "#fff",
+    padding: 20,
   },
   title: {
     fontSize: 24,
-    fontWeight: 'bold'
+    fontWeight: "bold",
   },
   input: {
     borderWidth: 1,
-    borderColor: '#ddd',
+    borderColor: "#ddd",
     padding: 10,
     fontSize: 18,
     borderRadius: 6,
-    marginBottom: 10
-  }  
+    marginBottom: 10,
+  },
 });
